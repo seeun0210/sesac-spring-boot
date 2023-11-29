83ce0083cf698ffe5c12901a3f0bd167de350e1d
kimseeun <cocobell3@naver.com>
2023-11-29 16:38:25 +0900

    first commit

---
+++ src/main/java/sesac/sesacspringboot/SesacSpringBootApplication.java
@@ -0,0 +1,13 @@
+package sesac.sesacspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesacSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesacSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sesac/sesacspringboot/SesacSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package sesac.sesacspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesacSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
